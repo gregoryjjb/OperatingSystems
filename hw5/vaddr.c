ad4fb4eac67616e52176133b9f6cca8697c3232a
Greg Brisebois <gregoryjjb@gmail.com>
2018-11-25 19:20:38 -0800

    Finished hw5 extra credit

--- hw5/vaddr.c
+++ hw5/vaddr.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define KB 1024
+
 int main(int argc, char const *argv[])
 {
-    char* devnull = NULL;
-    
-    long n = 0;
-    long v_addr = 0;
-    
     printf("Virtual address translation by Greg Brisebois 004\n");
     
-    n = strtol(argv[1], &devnull, 0);
-    v_addr = strtol(argv[2], &devnull, 0);
+    if(argc < 3) {
+        printf("Please pass in 2 numbers\n");
+        return 1;
+    }
     
-    printf("V addr = %d, n = %n\n", v_addr, n);
+    char* none;
+    
+    int n = strtol(argv[1], &none, 0);
+    int virtual_address = strtol(argv[2], &none, 0);
+    
+    if(n < 1) {
+        printf("N must be >= 1\n");
+        return 1;
+    }
+    
+    int page_size = KB * n;
+    int page_num = virtual_address / page_size;
+    int offset = virtual_address - (page_num * page_size);
+    
+    printf("Page size = %d, virtual address %d => page number => %d, offset = %d\n", page_size, virtual_address, page_num, offset);
     
     return 0;
 }
